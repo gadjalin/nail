a879c6473f586ea137db77da03a7143ce4dce96c
- The Aarnold - <mane.imation9@gmail.com>
2023-05-23 16:33:58 +0200

    Reorganise macros + version control

--- include/nail/defines.hpp
+++ include/nail/defines.hpp
@@ -1,63 +1,74 @@
+// defines.hpp
+// 27 Nov 2020
+// Gaétan "Gad" Jalin
+// See end of file for complete licence description
 #ifndef NAIL_DEFINES_HPP
 #define NAIL_DEFINES_HPP
 
-#include <string>
-#include <filesystem>
-#include <fmt/format.h>
+// Utilities
+#define NAIL_BIT(shift) (1 << shift)
 
-#include "platform.hpp"
+#define NAIL_TOSTRING(x) NAIL_TOSTRING_(x)
+#define NAIL_TOSTRING_(x) #x
 
-#define NAIL_VERSION_MAJOR 0
-#define NAIL_VERSION_MINOR 0
-#define NAIL_VERSION_REV 1
-#define NAIL_VERSION_STAGE "pre"
+#define NAIL_CONCAT(a, b) NAIL_CONCAT_(a, b)
+#define NAIL_CONCAT_(a, b) a##b
 
-#define FLAG(shift) (1 << shift)
-#define MACRO_TO_STRING(x) TO_STRING(x)
-#define TO_STRING(str) #str
-
-#ifdef NAIL_PLATFORM_WINDOWS
-    #define NAIL_FILEPATHSEPARATOR_DOS
+// DLL/SO symbol visibility
+// Note: The CMake files have not been made to handle DLL yet, but leaves the
+// door open
+#if defined(_WIN32) && defined(NAIL_BUILD_DLL)
+    #define NAIL_API __declspec(dllexport)
+    #define NAIL_HIDDEN
+#elif defined(_WIN32) && defined(NAIL_LINK_DLL)
+    #define NAIL_API __declspec(dllimport)
+    #define NAIL_HIDDEN
+#elif __GNUC__ >= 4 || defined(__clang__)
+    #define NAIL_API __attribute__((visibility("default")))
+    #define NAIL_HIDDEN __attribute__((visibility("hidden")))
 #else
-    #define NAIL_FILEPATHSEPARATOR_UNIX
-#endif
+    #define NAIL_API
+    #define NAIL_HIDDEN
+#endif // DLL/SO symbol visibility
 
-#ifdef NAIL_FILEPATHSEPARATOR_DOS
-    #define __FILENAME__ \
-        (std::string_view(__FILE__).find_last_of('\\') != std::string_view::npos ? \
-        (char const*) __FILE__ + std::string_view(__FILE__).find_last_of('\\') + 1 : __FILE__)
-#else
-    #define __FILENAME__ \
-        (std::string_view(__FILE__).find_last_of('/') != std::string_view::npos ? \
-        (char const*) __FILE__ + std::string_view(__FILE__).find_last_of('/') + 1 : __FILE__)
-#endif
-
-#define CURRENT_SOURCE_LOCATION SourceLocation{__FILENAME__, static_cast<unsigned int>(__LINE__), __func__ }
-
-typedef struct
-{
-    char const* file;
-    unsigned int line;
-    char const* function;
-} SourceLocation;
-
-template <>
-struct fmt::formatter<std::filesystem::path>
-{
-    constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin())
-    {
-        auto it = ctx.begin(), end = ctx.end();
-        if (it != end && *it != '}')
-            throw format_error("invalid format");
-        return it;
-    }
- 
-    template<typename FormatContext>
-    auto format(std::filesystem::path const& path, FormatContext& ctx) -> decltype(ctx.out())
-    {
-        return format_to(ctx.out(), "{}", path.string());
-    }
-};
+//template <>
+//struct fmt::formatter<std::filesystem::path>
+//{
+//    constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin())
+//    {
+//        auto it = ctx.begin(), end = ctx.end();
+//        if (it != end && *it != '}')
+//            throw format_error("invalid format");
+//        return it;
+//    }
+// 
+//    template<typename FormatContext>
+//    auto format(std::filesystem::path const& path, FormatContext& ctx) -> decltype(ctx.out())
+//    {
+//        return format_to(ctx.out(), "{}", path.string());
+//    }
+//};
 
 #endif // NAIL_DEFINES_HPP
+/**
+ * Copyright (C) 2020-2023 Gaétan Jalin
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *    1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ *    2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ *
+ *    3. This notice may not be removed or altered from any source distribution.
+ **/
 
---
+++ src/version.cpp
@@ -0,0 +1,25 @@
+#include "nail/version.hpp"
+
+//using namespace std; lol jk
+
+#define NAIL_MAKE_VERSION_STRING(major, minor, rev, stage) NAIL_CONCAT_VERSION_STRING(major, minor, rev, stage)
+#define NAIL_CONCAT_VERSION_STRING(major, minor, rev, stage) #major "-" #minor "." #rev stage
+
+namespace nail
+{
+    Version GetLibraryVersion() noexcept
+    {
+        return {
+            .major = NAIL_VERSION_MAJOR,
+            .minor = NAIL_VERSION_MINOR,
+            .rev = NAIL_VERSION_REV,
+            .stage = NAIL_VERSION_STAGE
+        };
+    }
+
+    char const* GetLibraryVersionString() noexcept
+    {
+        return NAIL_MAKE_VERSION_STRING(NAIL_VERSION_MAJOR, NAIL_VERSION_MINOR, NAIL_VERSION_REV, NAIL_VERSION_STAGE);
+    }
+}
+
