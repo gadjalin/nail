31356b70137334f112b72edd1e3d41862f018007
- The Aarnold - <mane.imation9@gmail.com>
2023-07-31 22:44:07 +0200

    Add underlying type

--- include/nail/StrongType/NamedType.hpp
+++ include/nail/StrongType/NamedType.hpp
@@ -16,9 +16,12 @@ namespace nail
     class NamedType
     {
     public:
+        using ValueType = Underlying;
+
         Underlying& get() noexcept;
         Underlying const& get() const noexcept;
 
+        NamedType() = default;
         explicit NamedType(Underlying const& value);
         template<typename T = Underlying, typename = typename std::enable_if_t<!std::is_reference_v<T> > >
         explicit NamedType(Underlying&& value);
